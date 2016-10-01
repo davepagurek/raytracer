bcfaf3ca85c54c1fa4cf6e5f9a0a66a191394f62
Dave Pagurek <davepagurek@gmail.com>
2016-09-30 20:05:57 -0700

    Initial Commit

---
+++ raytracer/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  raytracer
+//
+//  Created by David Pagurek van Mossel on 9/30/16.
+//  Copyright © 2016 David Pagurek van Mossel. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
